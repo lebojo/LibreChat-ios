f40a95399bec4dd39ecd692b4e4b12dd95c6f164
LeBojo <contact@jordan-chap.ch>
2025-10-19 02:23:09 +0200

    init

---
+++ LibreChat-ios/LibreChat_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibreChat_iosApp.swift
+//  LibreChat-ios
+//
+//  Created by Jordan Chap on 19.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct LibreChat_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
