af652e6fb8084a704f7af5332b2b9484c1d8e059
LeBojo <contact@jordan-chap.ch>
2026-05-17 19:28:55 +0200

    feat: Keyboard shortcut for send

--- LibreChat-ios/Views/ComposeBar.swift
+++ LibreChat-ios/Views/ComposeBar.swift
@@ -39,6 +39,7 @@ struct ComposeBar: ViewModifier {
                         onSend()
                     }
                     .disabled(!canSend)
+                    .keyboardShortcut(.defaultAction)
                 }
             }
     }
